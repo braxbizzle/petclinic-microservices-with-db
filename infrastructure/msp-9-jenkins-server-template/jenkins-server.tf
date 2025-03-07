6587e419644e59991708168fbee9b5bce34e7eef
Braxton Payton <braxtonpayton24@gmail.com>
2025-03-07 01:10:01 +0000

    updated jenkins server file

--- infrastructure/msp-9-jenkins-server-template/jenkins-server.tf
+++ infrastructure/msp-9-jenkins-server-template/jenkins-server.tf
@@ -52,6 +52,12 @@ resource "aws_security_group" "tf-jenkins-sec-gr" {
     to_port     = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8000
+    protocol    = "tcp"
+    to_port     = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
